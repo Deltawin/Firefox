9c4b289c872aa3c802ff5b31450f76552227243a
SkyWin <alexandre59135@outlook.fr>
2019-04-06 21:32:06 +0000

    Update autoconfig.js

--- MozillaCFG/autoconfig.js
+++ MozillaCFG/autoconfig.js
@@ -1,7 +1,7 @@
 pref("general.config.filename", "mozilla.cfg");
 pref("general.config.obscure_value", 0);
 
-// Performance
+//PERFORMANCE
 pref("layout.frame_rate.precise", true);
 pref("webgl.force-enabled", true);
 pref("layers.acceleration.force-enabled", true);
@@ -9,34 +9,3 @@ pref("layers.offmainthreadcomposition.enabled", true);
 pref("layers.offmainthreadcomposition.async-animations", true);
 pref("layers.async-video.enabled", true);
 pref("html5.offmainthread", true);
-
-// Memory reduction
-// Cache disk
-pref("browser.cache.disk.capacity", 0);
-pref("browser.cache.disk.enable", false);
-pref("browser.cache.disk.filesystem_reported", 0);
-pref("browser.cache.disk.free_space_hard_limit", 0);
-pref("browser.cache.disk.free_space_soft_limit", 0);
-pref("browser.cache.disk.max_chunks_memory_usage", 0);
-pref("browser.cache.disk.max_entry_size", 0);
-pref("browser.cache.disk.max_priority_chunks_memory_usage", 0);
-pref("browser.cache.disk.metadata_memory_limit", 0);
-pref("browser.cache.disk.preload_chunk_count", 0);
-pref("browser.cache.disk.smart_size.enabled", false);
-pref("browser.cache.disk.smart_size.first_run", false);
-pref("browser.cache.disk.smart_size.use_old_max", false);
-pref("browser.cache.disk_cache_ssl", false);
-// Cache memory
-pref("browser.cache.memory.capacity", 0);
-pref("browser.cache.memory.enable", false);
-pref("browser.cache.memory.max_entry_size", 0);
-// Cache offline
-pref("browser.cache.offline.capacity", 0);
-pref("browser.cache.offline.enable", false);
-pref("browser.cache.offline.insecure.enable", false);
-
-pref("browser.sessionhistory.max_entries", 5);
-pref("browser.sessionstore.max_tabs_undo", 5);
-pref("browser.tabs.animate", false);
-pref("browser.download.animateNotifications", false);
-pref("config.trim_on_minimize", true);

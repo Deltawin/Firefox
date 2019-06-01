5d16d833f666ee8849a7ce055450b7f2fa657eae
Deltawin <alexandre59135@outlook.fr>
2019-06-01 22:05:50 +0200

    Update autoconfig.js

--- MozillaCFG/Firefox.57.et.superieur/autoconfig.js
+++ MozillaCFG/Firefox.57.et.superieur/autoconfig.js
@@ -9,12 +9,5 @@ pref("layers.offmainthreadcomposition.enabled", true);
 pref("layers.offmainthreadcomposition.async-animations", true);
 pref("layers.async-video.enabled", true);
 pref("html5.offmainthread", true);
-
-// MEMORY REDUCTION
-pref("browser.sessionhistory.max_entries", 30); // 50
-pref("browser.sessionstore.max_tabs_undo ", 5); // 10
-pref("browser.tabs.animate", false);
-pref("browser.download.animateNotifications", false);
-pref("image.mem.max_decoded_image_kb", 51200);
-pref("javascript.options.mem.max", 51200);
-pref("javascript.options.mem.high_water_mark", 30);
+pref("layout.css.servo.enabled", true);
+pref("network.security.esni.enabled", true);
